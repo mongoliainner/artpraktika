1bbaae5e111e94b9851bac02e777e3714d73ed19
= <yuexrender@gmail.com>
2024-06-28 08:15:07 +0300

    test: 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import styles from "./styles.module.css";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>fair</h1>
+      <Home></Home>
     </>
   );
 }
